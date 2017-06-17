1505982fea441fd58a9bbb31cdd9098826c8f96b
Ffloriel <florielfedry@gmail.com>
2017-06-17 19:25:23 +0100

    Add tests

--- __tests__/purifycss.test.js
+++ __tests__/purifycss.test.js
@@ -377,4 +377,254 @@ describe("purify methods with files", () => {
             expect(result.includes(".another-one-not-found")).toBe(false)
         })
     })
+
+    describe("camelCase", () => {
+        it("finds testFoo", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}camel_case/camel_case.js`],
+                css: [`${root}camel_case/camel_case.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("testFoo")).toBe(true)
+        })
+
+        it("finds camelCase", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}camel_case/camel_case.js`],
+                css: [`${root}camel_case/camel_case.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("camelCase")).toBe(true)
+        })
+    })
+
+    describe("wildcard", () => {
+        it("finds universal selector", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("*")).toBe(true)
+        })
+
+        it("finds :before", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("before")).toBe(true)
+        })
+
+        it("finds scrollbar", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("scrollbar")).toBe(true)
+        })
+
+        it("finds selection", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("selection")).toBe(true)
+        })
+
+        it("finds vertical", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("vertical")).toBe(true)
+        })
+
+        it("finds :root", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}wildcard/wildcard.html`],
+                css: [`${root}wildcard/wildcard.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes(":root")).toBe(true)
+        })
+    })
+
+    describe("media queries", () => {
+        it("finds .media-class", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes(".media-class")).toBe(true)
+        })
+
+        it("finds .alone", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes(".alone")).toBe(true)
+        })
+
+        it("finds #id-in-media", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("#id-in-media")).toBe(true)
+        })
+
+        it("finds body", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("body")).toBe(true)
+        })
+
+        it("removes .unused-class", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes(".unused-class")).toBe(false)
+        })
+
+        it("removes the empty media query", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}media_queries/media_queries.html`],
+                css: [`${root}media_queries/media_queries.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("66666px")).toBe(false)
+        })
+    })
+
+    describe("special characters", () => {
+        it("finds @home", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}special_characters/special_characters.js`],
+                css: [`${root}special_characters/special_characters.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("@home")).toBe(true)
+        })
+
+        it("finds +rounded", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}special_characters/special_characters.js`],
+                css: [`${root}special_characters/special_characters.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("+rounded")).toBe(true)
+        })
+
+        it("finds button", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}special_characters/special_characters.js`],
+                css: [`${root}special_characters/special_characters.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("button")).toBe(true)
+        })
+    })
+
+    describe("delimited", () => {
+        it("removes the extra comma", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}delimited/delimited.html`],
+                css: [`${root}delimited/delimited.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            var commaCount = result.split("").reduce((total, chr) => {
+                if (chr === ",") {
+                    return total + 1
+                }
+
+                return total
+            }, 0)
+
+            expect(commaCount).toBe(1)
+        })
+
+        it("finds h1", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}delimited/delimited.html`],
+                css: [`${root}delimited/delimited.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("h1")).toBe(true)
+        })
+
+        it("finds p", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}delimited/delimited.html`],
+                css: [`${root}delimited/delimited.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("p")).toBe(true)
+        })
+
+        it("removes .unused-class-name", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}delimited/delimited.html`],
+                css: [`${root}delimited/delimited.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes(".unused-class-name")).toBe(false)
+        })
+    })
+
+    describe("pseudo classes", () => {
+        it("finds div:before", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}pseudo_class/pseudo_class.js`],
+                css: [`${root}pseudo_class/pseudo_class.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("div:before")).toBe(true)
+        })
+
+        it("removes row:after", () => {
+            const purifycss = new PurifyCss({
+                content: [`${root}pseudo_class/pseudo_class.js`],
+                css: [`${root}pseudo_class/pseudo_class.css`],
+                legacy: true
+            })
+            const result = purifycss.purify()[0].css
+            expect(result.includes("row:after")).toBe(false)
+        })
+    })
 })
